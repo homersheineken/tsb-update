b9f06474010643667306ee3b50e3818e08e06934
MFazio23 <MFazio23@d7b0d7c8-e3e6-11dd-9059-71fac19e68c6>
2009-01-16 16:24:54 +0000

    Added "Rosters" folder, which currently includes the code for the "Madden to TSB" program, where the Madden rosters are taken from IGN and displayed.

---
+++ trunk/Rosters/Madden to TSB/src/types/Player.java
@@ -0,0 +1,46 @@
+package types;
+
+public class Player {
+	
+	Team _team;
+	String _firstName;
+	String _lastName;
+	String _position;
+	String _number;
+	String[] _attributes;
+	
+	public Player(){
+		_team = null;
+		_firstName = "Generic";
+		_lastName = "Player";
+		_position = "N/A";
+		_number = "0";
+		_attributes = null;
+	}
+	
+	public Player(Team team, String first, String last, String position, String number, String[] attributes){
+		_team = team;
+		_firstName = first;
+		_lastName = last;
+		_position = position;
+		_number = number;
+		_attributes = attributes;
+	}
+	
+	public String getFullName(){
+		return _firstName + " " + _lastName;
+	}
+	
+	public String getFirstName(){
+	    return _firstName;
+	}
+	
+	public String getLastName(){
+	    return _lastName;
+	}
+	
+	public String[] getAttributes(){
+	    return _attributes;
+	}
+
+}
