3b62883c24cd7212b66662cb9c025741b23d1ea0
MFazio23 <MFazio23@d7b0d7c8-e3e6-11dd-9059-71fac19e68c6>
2009-01-18 06:54:52 +0000

    Changed directory.

---
+++ Rosters/Madden to TSB/src/TSB/info/TeamList.java
@@ -0,0 +1,77 @@
+package TSB.info;
+
+public class TeamList {
+
+    public static final String[] ArizonaCardinals = {"Arizona","Cardinals"};
+    public static final String[] AtlantaFalcons = {"Atlanta","Falcons"};
+    public static final String[] BaltimoreRavens = {"Baltimore","Ravens"};
+	public static final String[] BuffaloBills = {"Buffalo","Bills"};
+    public static final String[] CarolinaPanthers = {"Carolina","Panthers"};
+    public static final String[] ChicagoBears = {"Chicago","Bears"};
+    public static final String[] CincinnatiBengals = {"Cincinnati","Bengals"};
+    public static final String[] ClevelandBrowns = {"Cleveland","Browns"};
+    public static final String[] DallasCowboys = {"Dallas","Cowboys"};
+    public static final String[] DenverBroncos = {"Denver","Broncos"};
+    public static final String[] DetroitLions = {"Detroit","Lions"};
+    public static final String[] GreenBayPackers = {"Green Bay","Packers"};
+    public static final String[] HoustonTexans = {"Houston","Texans"};
+    public static final String[] IndianapolisColts = {"Indianapolis","Colts"};
+    public static final String[] JacksonvilleJaguars = {"Jacksonville","Jaguars"};
+    public static final String[] KansasCityChiefs = {"Kansas City","Chiefs"};
+	public static final String[] MiamiDolphins = {"Miami","Dolphins"};
+    public static final String[] MinnesotaVikings = {"Minnesota","Vikings"};
+	public static final String[] NewEnglandPatriots = {"New England","Patriots"};
+    public static final String[] NewOrleansSaints = {"New Orleans","Saints"};
+    public static final String[] NewYorkGiants = {"New York","Giants"};
+	public static final String[] NewYorkJets = {"New York","Jets"};
+    public static final String[] OaklandRaiders = {"Oakland","Raiders"};
+    public static final String[] PhiladelphiaEagles = {"Philadelphia","Eagles"};
+	public static final String[] PittsburghSteelers = {"Pittsburgh","Steelers"};
+    public static final String[] SanDiegoChargers = {"San Diego","Chargers"};
+    public static final String[] SanFrancisco49ers = {"San Francisco","49ers"};
+    public static final String[] SeattleSeahawks = {"Seattle","Seahawks"};
+    public static final String[] StLouisRams = {"St. Louis","Rams"};
+    public static final String[] TampaBayBuccaneers = {"Tampa Bay","Buccaneers"};
+	public static final String[] TennesseeTitans = {"Tennessee","Titans"};
+	public static final String[] WashingtonRedskins = {"Washington","Redskins"};
+	
+	public static String[][] getAllTeams(){
+		String[][] allTeams =
+			{ArizonaCardinals,
+		        AtlantaFalcons,
+		        BaltimoreRavens,
+		        BuffaloBills,
+		        CarolinaPanthers,
+		        ChicagoBears,
+		        CincinnatiBengals,
+		        ClevelandBrowns,
+		        DallasCowboys,
+		        DenverBroncos,
+		        DetroitLions,
+		        GreenBayPackers,
+		        HoustonTexans,
+		        IndianapolisColts,
+		        JacksonvilleJaguars,
+		        KansasCityChiefs,
+		        MiamiDolphins,
+		        MinnesotaVikings,
+		        NewEnglandPatriots,
+		        NewOrleansSaints,
+		        NewYorkGiants,
+		        NewYorkJets,
+		        OaklandRaiders,
+		        PhiladelphiaEagles,
+		        PittsburghSteelers,
+		        SanDiegoChargers,
+		        SanFrancisco49ers,
+		        SeattleSeahawks,
+		        StLouisRams,
+		        TampaBayBuccaneers,
+		        TennesseeTitans,
+		        WashingtonRedskins	 
+			};
+		
+		return allTeams;
+	}
+
+}
---
+++ Rosters/Madden to TSB/src/TSB/types/Team.java
@@ -0,0 +1,47 @@
+package TSB.types;
+
+public class Team {
+	
+	private String _location;
+	private String _name;
+	private Player[] _players;
+	
+	public Team(){
+		_location = "Generic";
+		_name = "Team";
+	}
+	
+	public Team(String locationIn, String nameIn){
+		_location = locationIn;
+		_name = nameIn;
+	}
+	
+	public String getTeamName(){
+		return _name;
+	}
+	
+	public String getLocation(){
+		return _location;
+	}
+	
+	public void setTeamName(String team_name){
+		_name = team_name;
+	}
+	
+	public void setLocation(String loc){
+		_location = loc;
+	}
+	
+	public String getFullTeamName(){
+		return _location + " " + _name;
+	}
+	
+	public Player[] getPlayers(){
+		return _players;
+	}
+	
+	public void setPlayers(Player[] players){
+		_players = players;
+	}
+
+}
